64bdc294de2a74f970303790c1ba09104b31d387
Truong Nguyen <tnguyen@demystdata.com>
2020-04-22 23:46:55 +1000

    flatten data to become a normalize list

--- app.py
+++ app.py
@@ -7,8 +7,28 @@ from dotenv import load_dotenv
 import os
 import pymongo
 import time
+from bson.objectid import ObjectId
 
 load_dotenv()
+
+
+def extract_name(name):
+    units = ['Tỉnh', 'Thành phố', 'Quận', 'Huyện', 'Phường', 'Xã']
+    try:
+        for u in units:
+            if u in name:
+                return {
+                    'unit': u,
+                    'value': name.replace(u, '').strip()
+                }
+    except:
+        print(name)
+
+    return {
+        'unit': '',
+        'value': name
+    }
+
 
 mongoClient = pymongo.MongoClient(os.getenv("DB_CONNECTION_STRING"))
 db = mongoClient[os.getenv("DB_NAME")]
@@ -20,20 +40,57 @@ df = pd.read_excel(file, encoding=sys.getfilesystemencoding())
 # for r in df:
 #   print(r)
 data = []
+dict = {}
+items = []
 for index, row in df.iterrows():
+    province = extract_name(row["Tỉnh Thành Phố"])
+    district = extract_name(row["Quận Huyện"])
+    area = extract_name(row["Phường Xã"])
     item = {
-        'provine': row["Tỉnh Thành Phố"][5::],
+        '_id': ObjectId(),
+        'province': province['value'],
+        'provinceType': province['unit'],
         'code': row["Mã TP"],
-        'district': row["Quận Huyện"],
+        'district': district["value"],
+        'districtType': district['unit'],
         'districtCode': row["Mã QH"],
-        'area': row['Phường Xã'],
+        'area': area["value"],
+        # "areaType": area["unit"]
         'areaCode': row['Mã PX'],
         'areaType': row['Cấp']
     }
     print(item)
     data.append(item)
 
-batch_size = 500
+    if dict.get(item['province']) == None:
+        dict[item['province']] = ObjectId()
+        items.append({
+            'type': item["provinceType"],
+            '_id':  dict[item['province']],
+            'code': item['code'],
+            'name': item['province']
+        })
+    if dict.get(item['province'] + item['district']) == None:
+        dict[item['province']+item['district']] = ObjectId()
+        items.append({
+            'type': item['districtType'],
+            '_id':   dict[item['province']+item['district']],
+            'code': item['districtCode'],
+            'name': item['district'],
+            'parent_id': dict[item['province']]
+        })
+
+    items.append({
+        '_id':   ObjectId(),
+        'code': item['areaCode'],
+        'name': item['area'],
+        'type': item['areaType'],
+        'parent_id': dict[item['province']+item['district']]
+    })
+
+db.items.drop()
+db.items.insert_many(items)
+batch_size = 1000
 for index in range(0, len(data), batch_size):
     page = data[index:index+batch_size]
     start = time.process_time()
