84224cc02d902e4efaa2ca1be66b3b34a5f39aef
Truong Nguyen <tnguyen@demystdata.com>
2020-04-22 18:23:14 +1000

    MISC

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ import sys
 from dotenv import load_dotenv
 import os
 import pymongo
+import time
 
 load_dotenv()
 
 mongoClient = pymongo.MongoClient(os.getenv("DB_CONNECTION_STRING"))
 db = mongoClient[os.getenv("DB_NAME")]
 
-print(' Drop data collection')
-db.data.drop()
+print('Drop all data in collection')
+db.data.delete_many({})
 file = r'input.xls'
 df = pd.read_excel(file, encoding=sys.getfilesystemencoding())
 # for r in df:
@@ -32,6 +33,12 @@ for index, row in df.iterrows():
     print(item)
     data.append(item)
 
-for index in range(0, len(data), 100):
-    page = data[index:index+100]
+batch_size = 500
+for index in range(0, len(data), batch_size):
+    page = data[index:index+batch_size]
+    start = time.process_time()
     db.data.insert_many(page)
+    elapsed_time = time.process_time() - start
+
+    print(f'Write %d item from index: %d took : %d' %
+          (len(page), index, elapsed_time))
